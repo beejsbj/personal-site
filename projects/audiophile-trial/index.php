c9e32d296c03763fdc2d2346b31c0ab0222b20ec
beejsbj <burooj.bj@gmail.com>
2022-06-11 13:05:01 -0400

    more audiophile
    Signed-off-by: beejsbj <burooj.bj@gmail.com>

--- projects/audiophile-trial/index.php
+++ projects/audiophile-trial/index.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="[[XXXXXXXXXXXXXXXXX]]">
+	<meta property="og:image" content= "[XXXXXXXXX]">
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<title>Audiophile</title>
+</head>
+
+
+
+
+
 
 <?php
 function formatCode($code) {
@@ -20,204 +37,147 @@ $products = $audiophileData["products"];
 
 
 
-
-
-
-
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="[[XXXXXXXXXXXXXXXXX]]">
-	<meta property="og:image" content= "[XXXXXXXXX]">
-
-	<link rel="stylesheet" href="style.css">
-
-	<title>Document</title>
-	<style>
-		* {
-			box-sizing: border-box;
-		}
-
-		body {
-
-		}
-
-		a {
-			display: block;
-			text-decoration: none;
-			color: inherit;
-		}
-		p a {
-			display: inline;
-			color: blue;
-			text-decoration: underline;
-		}
-
-		picture {
-			display: block;
-			position: relative;
-		}
-
-		picture h2.show {
-			display: block;
-			position: absolute;
-			top: 0px;
-			right: 0px;
-			transform: rotateZ(45deg);
-			background-color: red;
-			color: white;
-		}
-
-		.hide {
-			display: none;
-		}
-
-		img {
-			display: block; 
-			width: 100%;
-			height: auto;
-		}
-		.product-list {
-			display: flex;
-			flex-wrap: wrap;
-			gap: 10px;
-		}
-		product-card {
-			display: block;
-			padding: 10px;
-			width: 500px;
-			border: 4px solid black;
-		}
-
-		h5 {
-			color: green;
-		}
-	</style>
-</head>
 <body>
-	<ul class="product-list">
+	<header class="site-header">
+
+		<section class="header-block1">
+			<div class="inner-column">
+				<nav>
+					<a href="#">CART</a>
+				</nav>
+			</div>
+		</section>
+
+		<section class="header-block2">
+			<div class="inner-column">
+				<h1>Audiophile</h1>
+			</div>
+		</section>
+
+	</header>
+
+	<main class="site-main">
+		
+
+		<section class="main-block1">
+
+
+			<filter-list>
+				<h2>Filter</h2>
+				<li>Sort</li>
+
+			</filter-list>
+
+
+			<div class="inner-column">
+				<ul class="product-list">
+
+					<?php
+
+					$cart = ['hello', 'apples'];
+
+					
 
 
 
 
+					foreach ($products as $product) {
+
+						$image = $product["image"];
+						$brand = $product["brand"];
+						$name =  $product["name"];
+						$color = $product["color"];
+						$category = $product["category"];
+						$tagline = $product["tagline"];
+						$description = $product["description"];
+						$price = $product["price"];
+
+						$saleTag = "hide";
+
+						if ($product["on-sale"]) {
+							$saleTag = "show";
+							
+						}
+
+						$features = $product["features"];
+
+					 ?>
+
+					<product-card>
+						<p class="<?=$saleTag?>">SALE</p>
+						<picture>
+							
+							<img src="<?=$image?>" alt="">
+						</picture>
+
+						
+						<card-description>
+							<product-name><?=$name?></product-name>
+							<product-brand><?=$brand?></product-brand>
+							<product-tagline><?=$tagline?></product-tagline>
+							<product-description><?=$description?></product-description>
 
 
 
-		<?php
+							<details>
+	    					<summary>Features</summary>
 
-		foreach ($products as $product) {
+							<?php
+							foreach ($features as $feature) {
+								echo "<li> $feature </li>";				
+							}
 
-			$image = $product["image"];
-			$brand = $product["brand"];
-			$name =  $product["name"];
-			$color = $product["color"];
-			$category = $product["category"];
-			$tagline = $product["tagline"];
-			$description = $product["description"];
-			$price = $product["price"];
+							if (isset($_POST['addToCart'])) {
+						
+								array_push($cart, $name);
+							}?>
 
-			$saleTag = "hide";
-
-			if ($product["on-sale"]) {
-				$saleTag = "show";
-				
-			}
-
-			$features = $product["features"];
-
-
-
-			
+							</details>
+							<div class="flex-button-parent">
+								<product-price>$<?=$price?></product-price>
+								<button type="submit" name="addToCart">Add to card</button>
+							</div>
+						</card-description>
+					</product-card>
 
 
 
+					<?php } ?>
+				</ul>
+			</div>
 
-			// echo "] <br><br><br>";
-
-
-		 ?>
-
-		<product-card>
-			<picture>
-				<h2 class="<?=$saleTag?>">SALE</h2>
-				<img src="<?=$image?>" alt="">
-			</picture>
-			<h2><?=$name?></h2>
-			<h3><?=$brand?></h3>
-			<h4><?=$tagline?></h4>
-			<p><?=$description?></p>
-
-
-			<h5>Features</h5>
-			<ul>
-
-
-
+			<cart-list>
+				<h2>CART</h2>
 				<?php
 
-			foreach ($features as $feature) {
 
-				echo "<li> $feature </li>";
+				if (count($cart) > 0) {
+					foreach ($cart as $item) {
+
+					echo "<li>$item</li>";
 					
-			}
+					}
+				}
 
 
 				?>
-			</ul>
-			<h5><?=$price?></h5>
 
-		</product-card>
+			</cart-list>
 
 
+		</section>
 
-		<?php } ?>
-	</ul>
+		
+	</main>
+
+	<footer class="site-footer">
+
+		<section class="footer-block1">
+			<div class="inner-column">
+				
+			</div>
+		</section>
+
+	</footer>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
