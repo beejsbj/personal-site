5977a71121922cd9da396519dffd632c87b8b9f2
beejsbj <burooj.bj@gmail.com>
2022-07-01 22:57:21 -0400

    undo login

--- projects/php-crud/modules/header/index.php
+++ projects/php-crud/modules/header/index.php
@@ -1,13 +1,14 @@
+
+
+
+
 <section class="nav-bar">
 	<inner-column>
 		<nav class="site-menu">
-			//loop
+			//loop 
 			<a class="<?=activePage('home')?>" href="?page=home">Home</a>
 			<a class="<?=activePage('list')?>" href="?page=list">Jolly Roger List</a>
-			<?php 
-				if ($_POST["password"] == $_SESSION['name']) { ?>
-					<a class="<?=activePage('add')?>" href="?page=add">Add</a>
-			<?php } ?>
+			<a class="<?=activePage('add')?>" href="?page=add">Add</a>
 			<a class="<?=activePage('login')?>" href="?page=login">login</a>
 		</nav>
 	</inner-column>
