e39b7a2dd43cdfa9f8e6062fbd89cfb2af1ddbaa
beejsbj <burooj.bj@gmail.com>
2022-07-20 23:40:43 +0530

    layout 2 of day3

--- projects/responsive-layout-garden/index.php
+++ projects/responsive-layout-garden/index.php
@@ -23,10 +23,10 @@
 
 		<main class="page-content">
 
-			<?php //include('modules/layout1/layout1.php'); ?>
-			<?php //include('modules/layout2/layout2.php'); ?>
+			<?php include('modules/layout1/layout1.php'); ?>
+			<?php include('modules/layout2/layout2.php'); ?>
 			<?php include('modules/layout3/layout3.php'); ?>
-
+			<?php include('modules/layout4/layout4.php'); ?>
 		</main>
 
 		<footer>
