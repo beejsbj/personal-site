270b5cbd2d69ac94b0bbed1ba03d2eccf92ab22a
beejsbj <burooj.bj@gmail.com>
2022-07-24 20:53:22 +0530

    fix password field

--- projects/e4p/password-validation.php
+++ projects/e4p/password-validation.php
@@ -91,7 +91,7 @@ if (
                     <label for="username-ID">Username</label>
                 </div>
                 <div class="input-field">
-                    <input id="password-ID" type="password" name="password" placeholder="password?!" value="">
+                    <input class="text-number-input" id="password-ID" type="password" name="password" placeholder="password?!" value="">
                     <label for="password-ID">password</label>
                 </div>
 
