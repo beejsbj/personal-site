7f028e3a16f37ef075b07f1b15442cc7340d4d74
beejsbj <burooj.bj@gmail.com>
2022-08-27 18:03:02 -0400

    Added metadata variables

--- projects/personal-site/templates/modules/header/header.php
+++ projects/personal-site/templates/modules/header/header.php
@@ -5,15 +5,14 @@
 		<meta charset='utf-8'>
 		<meta name='viewport' content='width=device-width, initial-scale=1'>
 		<title><?=$pageData['title']?></title>
-		<meta name="description" content="Adventureeee">
-		<meta property="og:image" content="https://thumbs.dreamstime.com/b/woman-stands-crossroads-two-forest-roads-girl-choice-path-lost-woods-ways-to-achieve-goal-157422242.jpg">
+		<meta name="description" content="<?=$pageData['metaDesc']?>">
+		<meta property="og:image" content="<?=$pageData['metaImage']?>">
 		<link rel='stylesheet' href='styles/site.css'>
 	</head>
 
 	<body>
 		<header>
 			<inner-column>
-				<h1 class='loud-voice'><em>Dynamic</em> portfolio adventure</h1>
 				<?php include('templates/modules/site-menu/site-menu.php'); ?>
 			</inner-column>
 		</header>
