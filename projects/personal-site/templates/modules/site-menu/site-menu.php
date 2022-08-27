81563cb3537ccdc29bb78e1b9d840f7f8b42fdd6
beejsbj <burooj.bj@gmail.com>
2022-08-27 18:03:52 -0400

    Added links for new pages

--- projects/personal-site/templates/modules/site-menu/site-menu.php
+++ projects/personal-site/templates/modules/site-menu/site-menu.php
@@ -4,7 +4,22 @@
 			<a href='?page=home'>Home</a>
 		</li>
 		<li>
+			<a href='?page=about'>About</a>
+		</li>
+		<li>
 			<a href='?page=list'>List</a>
+		</li>
+		<li>
+			<a href='?page=goals'>goals</a>
+		</li>
+		<li>
+			<a href='?page=resume'>resume</a>
+		</li>
+		<li>
+			<a href='?page=projects'>projects</a>
+		</li>
+		<li>
+			<a href='?page=e4p'>e4p</a>
 		</li>
 		<li>
 			<a href='?page=style-guide'>Style guide</a>
