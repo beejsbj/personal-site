dc8015f2b8bf5ebe4105e9a4855b9d8cdb7b58be
beejsbj <burooj.bj@gmail.com>
2022-08-27 18:02:44 -0400

    Modify goals list

--- projects/personal-site/templates/modules/goals-list/goals-list.php
+++ projects/personal-site/templates/modules/goals-list/goals-list.php
@@ -1,10 +1,15 @@
+<goals-module>
+	<h2 class="attention-voice">
+	<?=$sectionHeading?>
+</h2>
 <ul >
 
 	<?php
 		$items = $section['items'];
-	 	foreach ($items as $item) {
+	 	foreach ($items as $goal) {
 		echo "<li>";
-		echo $item;
+		echo $goal;
 		echo "</li>";
 	} ?>
 </ul>
+</goals-module>
