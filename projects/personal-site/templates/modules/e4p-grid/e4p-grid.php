c357676c1013a1ea4e4147557e326aefc56fe73d
beejsbj <burooj.bj@gmail.com>
2022-08-28 11:10:47 -0400

    misc and formatting

--- projects/personal-site/templates/modules/e4p-grid/e4p-grid.php
+++ projects/personal-site/templates/modules/e4p-grid/e4p-grid.php
@@ -9,41 +9,36 @@
 <e4p-grid>
 
 	<?php
-	$chapters = $section['chapters'] ?? [1, 2, 3];
-	foreach ($chapters as $chapter) {
-	$chapterHeading = $chapter['heading'] ?? "E4P chapter heading";
-	$chapterDesc = $chapter['description']  ?? "E4P chapter description";
-	$chapterSvg = $chapter['svg']  ?? "xyz.svg";
-	$chapterExercises = $chapter['exercises'] ?? [1, 2, 3, 4];?>
+		$chapters = $section['chapters'] ?? [1, 2, 3];
+		foreach ($chapters as $chapter) {
+			$chapterHeading = $chapter['heading'] ?? "E4P chapter heading";
+			$chapterDesc = $chapter['description']  ?? "E4P chapter description";
+			$chapterSvg = $chapter['svg']  ?? "xyz.svg";
+			$chapterExercises = $chapter['exercises'] ?? [1, 2, 3, 4];?>
 
-	<e4p-chapter>
+			<e4p-chapter>
+				<h2 class='attention-voice'>
+				<?=$chapterHeading?>
+				</h2>
 
-	
-		<h2 class='attention-voice'>
-		<?=$chapterHeading?>
-		</h2>
+				<p>
+				<?=$chapterDesc?>
+				</p>
 
-		<p>
-		<?=$chapterDesc?>
-		</p>
+				<ul>
+				<?php
+				 	foreach ($chapterExercises as $exercise) {
+				 		$exName = $exercise['name'] ?? "exercise name";
+				 		$exNumber = $exercise['number']  ?? "exercise Number";
+				 		$exDesc = $exercise['description']  ?? "exercise chapterDesc";
+				 		$exLink = dasher($exName);
+				 		?>
 
-		<ul>
-		<?php
-		 	foreach ($chapterExercises as $exercise) {
-		 		$exName = $exercise['name'] ?? "exercise name";
-		 		$exNumber = $exercise['number']  ?? "exercise Number";
-		 		$exDesc = $exercise['description']  ?? "exercise chapterDesc";
-		 		$exLink = dasher($exName);
-		 		?>
-
-				<li>
-					<a href="?page=exercise&exercise=<?=$exLink?>"><?=$exName?></a>
-				</li>
-		<?php } ?>
-		</ul>
-
-		
-
-	</e4p-chapter>
+						<li>
+							<a href="?page=exercise&exercise=<?=$exLink?>"><?=$exName?></a>
+						</li>
+				<?php } ?>
+				</ul>
+			</e4p-chapter>
 	<?php } ?>
 </e4p-grid>
