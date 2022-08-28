6fedd0eeb1e32f1a75d285ba2deb955d7343d39a
beejsbj <burooj.bj@gmail.com>
2022-08-27 20:19:54 -0400

    Added exercise contigency for styleguide

--- projects/personal-site/templates/modules/exercise-detail/exercise-detail.php
+++ projects/personal-site/templates/modules/exercise-detail/exercise-detail.php
@@ -1,7 +1,7 @@
 <?php 
 // if heading exists condition
 	$sectionHeading = $section['heading'] ?? "This is the E4P module";
-	$exName = $_GET['exercise'];
+	$exName = $_GET['exercise'] ?? "saying-hello";
 ?>
 
 <h2 class="attention-voice">
