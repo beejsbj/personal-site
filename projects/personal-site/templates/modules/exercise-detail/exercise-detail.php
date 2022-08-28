d1758ce31b24ae4bc817533f437c0db8785afd98
beejsbj <burooj.bj@gmail.com>
2022-08-27 20:15:36 -0400

    Init exercise detial module

---
+++ projects/personal-site/templates/modules/exercise-detail/exercise-detail.php
@@ -0,0 +1,13 @@
+<?php 
+// if heading exists condition
+	$sectionHeading = $section['heading'] ?? "This is the E4P module";
+	$exName = $_GET['exercise'];
+?>
+
+<h2 class="attention-voice">
+	<?=$exName?>
+</h2>
+<?php 
+	include("projects/exercises/$exName.php");
+?>
+
