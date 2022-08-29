313ecef9277e54f175881a1590a780f95a0c1952
beejsbj <burooj.bj@gmail.com>
2022-08-28 21:09:18 -0400

    misc

--- projects/personal-site/templates/modules/section-test/section-test.php
+++ projects/personal-site/templates/modules/section-test/section-test.php
@@ -1,14 +1,14 @@
-<section class='page-section'>
-	<inner-column>
-		<div class="test">
-			<text-content>
-				<h2 class="attention-voice">
-					<?=$sectionHeading?>
-				</h2>
-			</text-content>
-			<picture>
-				<img src="images/square.jpg" alt="">
-			</picture>
-		</div>
-	</inner-column>
-</section>
+<?php 
+// if heading exists condition
+	$sectionHeading = $section['heading'] ?? "You are lost 404040";
+?>
+<div class="test">
+	<text-content>
+		<h2 class="attention-voice">
+			<?=$sectionHeading?>
+		</h2>
+	</text-content>
+	<picture>
+		<img src="images/landscape.jpg" alt="">
+	</picture>
+</div>
