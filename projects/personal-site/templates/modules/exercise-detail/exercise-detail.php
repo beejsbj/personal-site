734fba303c391b3efc037f48b03b91824f2d4118
beejsbj <burooj.bj@gmail.com>
2022-08-28 20:07:22 -0400

    Changed variable names

--- projects/personal-site/templates/modules/exercise-detail/exercise-detail.php
+++ projects/personal-site/templates/modules/exercise-detail/exercise-detail.php
@@ -1,13 +1,14 @@
 <?php 
 // if heading exists condition
 	$sectionHeading = $section['heading'] ?? "This is the E4P module";
-	$exName = $_GET['exercise'] ?? "saying-hello";
+	$exLink = $_GET['exercise'] ?? "saying-hello";
+	$exName = unDasher($exLink);
 ?>
 
 <h2 class="attention-voice">
 	<?=$exName?>
 </h2>
 <?php 
-	include("projects/exercises/$exName.php");
+	include("projects/e4p/$exLink.php");
 ?>
 
