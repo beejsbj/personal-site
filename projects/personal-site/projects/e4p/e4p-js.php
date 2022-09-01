e4dcffa21a0b3bb870b8045504eb4411bca58a7e
beejsbj <burooj.bj@gmail.com>
2022-09-01 18:29:43 -0400

    Create links menu for exercises

---
+++ projects/personal-site/projects/e4p/e4p-js.php
@@ -0,0 +1,27 @@
+<ul>
+	<li>
+		<button class="saying-hello">Saying Hello</button>
+	</li>
+	<li>
+		<button class="counting-number-of-characters">Counting the Number of Characters</button>
+	</li>
+	<li>
+		<button class="printing-quotes">Printing Quotes</button>
+	</li>
+	<li>
+		<button class="mad-lib">Mad Lib</button>
+	</li>
+	<li>
+		<button class="simple-math">Simple Math</button>
+	</li>
+	<li>
+		<button class="retirement-calculator">Retirement Calculator</button>
+	</li>
+	<li>
+		<button class="area-of-a-rectangular-room">Area of Rectangular Room</button>
+	</li>
+	<li>
+		<button class="pizza-party">Pizza Party</button>
+	</li>
+	<li>
+		<button class="paint-calculator">Paint Calculator</button>
