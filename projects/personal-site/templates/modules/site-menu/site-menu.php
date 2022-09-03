7253a6f494757faaba54995ab9a2645cd090bba9
beejsbj <burooj.bj@gmail.com>
2022-09-03 12:04:02 -0400

    Hide extra test links

--- projects/personal-site/templates/modules/site-menu/site-menu.php
+++ projects/personal-site/templates/modules/site-menu/site-menu.php
@@ -1,20 +1,24 @@
 <nav class="site-menu">
 	<ul>
 		<li>
-			<a href='?page=home'>Home</a>
+			<a href='?page=home'>home</a>
 		</li>
 		<li>
-			<a href='?page=about'>About</a>
+			<a href='?page=about'>about</a>
 		</li>
 		<li>
+			<a href='?page=projects'>projects</a>
+		</li>
+		<li>
+			<a href='?page=style-guide'>style guide</a>
+		</li>
+		<!-- <li>
 			<a href='?page=goals'>goals</a>
 		</li>
 		<li>
 			<a href='?page=resume'>resume</a>
 		</li>
-		<li>
-			<a href='?page=projects'>projects</a>
-		</li>
+		
 		<li>
 			<a href='?page=e4p'>e4p</a>
 		</li>
@@ -22,10 +26,7 @@
 			<a href='?page=layout-garden'>Garden</a>
 		</li>
 		<li>
-			<a href='?page=style-guide'>Style guide</a>
-		</li>
-		<li>
 			<a href='?page=dfghdfg'>404</a>
-		</li>
+		</li> -->
 	</ul>
 </nav>
