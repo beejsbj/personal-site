8f37bfcf61e4d4768a171df025aca14be18d9a36
beejsbj <burooj.bj@gmail.com>
2022-09-03 13:35:06 -0400

    Created theme menu

---
+++ projects/personal-site/templates/components/theme-menu/theme-menu.php
@@ -0,0 +1,16 @@
+<nav class="theme-menu">
+	<ul>
+		<li>
+			<a href='?theme=default'>Default</a>
+		</li>
+		<li>
+			<a href='?theme=theme2'>theme abc</a>
+		</li>
+		<li>
+			<a href='?theme=theme2'>theme theme</a>
+		</li>
+		<li>
+			<a href='?theme=theme2'>theme</a>
+		</li>
+	</ul>
+</nav>
