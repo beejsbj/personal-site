10c86badc2841a3eefedc61ca82404415ce20d37
beejsbj <burooj.bj@gmail.com>
2022-09-03 14:11:02 -0400

    Added Svg and styled it

--- projects/personal-site/templates/modules/header/header.php
+++ projects/personal-site/templates/modules/header/header.php
@@ -13,11 +13,16 @@
 	</head>
 
 	<body>
-		<header class="hide">
-			<inner-column>
+		<header>
+
+				<svg class="circle" viewBox="0 0 100 100">
+				  <circle cx="50" cy="50" r="40" />
+				</svg> 
+
+			<inner-column class="hide">
 				<?php include('templates/components/site-menu/site-menu.php'); ?>
 			</inner-column>
-			<inner-column>
+			<inner-column class="hide">
 				<?php include('templates/components/theme-menu/theme-menu.php'); ?>
 			</inner-column>
 		</header>
