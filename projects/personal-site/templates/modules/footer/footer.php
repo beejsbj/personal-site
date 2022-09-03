5acd11195a09b43e7f4a734ad47a8814870a21cb
beejsbj <burooj.bj@gmail.com>
2022-09-03 12:01:18 -0400

    Removed footer placeholder header

--- projects/personal-site/templates/modules/footer/footer.php
+++ projects/personal-site/templates/modules/footer/footer.php
@@ -1,8 +1,5 @@
 <footer>
 	<inner-column>
-		<h2 class="attention-voice">Zee footah</h2>
-	</inner-column>
-	<inner-column>
 		<?php include('templates/modules/site-menu/site-menu.php'); ?>
 	</inner-column>
 </footer>
