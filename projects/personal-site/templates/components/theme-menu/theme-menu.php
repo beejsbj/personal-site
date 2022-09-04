83058068410aebe6bd800e7de152d179a46eab11
beejsbj <burooj.bj@gmail.com>
2022-09-04 19:38:18 -0400

    Modified theme menu to maintain current page

--- projects/personal-site/templates/components/theme-menu/theme-menu.php
+++ projects/personal-site/templates/components/theme-menu/theme-menu.php
@@ -1,16 +1,16 @@
 <nav class="theme-menu">
 	<ul>
 		<li>
-			<a href='?theme=default'>Default</a>
+			<a href='?page=<?=currentPage()?>&theme=default'>Default</a>
 		</li>
 		<li>
-			<a href='?theme=theme2'>theme abc</a>
+			<a href='?page=<?=currentPage()?>&theme=theme2'>theme abc</a>
 		</li>
 		<li>
-			<a href='?theme=theme2'>theme</a>
+			<a href='?page=<?=currentPage()?>&theme=theme3'>theme</a>
 		</li>
 		<li>
-			<a href='?theme=theme2'>theme thyme</a>
+			<a href='?page=<?=currentPage()?>&theme=theme4'>theme thyme</a>
 		</li>
 	</ul>
 </nav>
