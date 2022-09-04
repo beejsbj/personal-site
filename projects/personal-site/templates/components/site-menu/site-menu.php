df3e604cf82d8ddf51d9ea939618e1a9eda90a20
beejsbj <burooj.bj@gmail.com>
2022-09-04 14:35:52 -0400

    Added support for persistant themes across links

--- projects/personal-site/templates/components/site-menu/site-menu.php
+++ projects/personal-site/templates/components/site-menu/site-menu.php
@@ -1,16 +1,16 @@
 <nav class="site-menu">
 	<ul>
 		<li>
-			<a class="<?=activePage('home')?>" href='?page=home'><span>home</span></a>
+			<a class="<?=activePage('home')?>" href='?page=home&theme=<?=currentTheme()?>'><span>home</span></a>
 		</li>
 		<li>
-			<a class="<?=activePage('about')?>" href='?page=about'><span>about</span></a>
+			<a class="<?=activePage('about')?>" href='?page=about&theme=<?=currentTheme()?>'><span>about</span></a>
 		</li>
 		<li>
-			<a class="<?=activePage('projects')?>" href='?page=projects'><span>projects</span></a>
+			<a class="<?=activePage('projects')?>" href='?page=projects&theme=<?=currentTheme()?>'><span>projects</span></a>
 		</li>
 		<li>
-			<a class="<?=activePage('style-guide')?>" href='?page=style-guide'><span>style guide</span></a>
+			<a class="<?=activePage('style-guide')?>" href='?page=style-guide&theme=<?=currentTheme()?>'><span>style guide</span></a>
 		</li>
 		<!-- <li>
 			<a href='?page=goals'>goals</a>
