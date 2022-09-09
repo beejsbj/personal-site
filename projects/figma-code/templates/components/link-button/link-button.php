d570d608fde71b9662abeee6966ba57e9eea2b91
beejsbj <burooj.bj@gmail.com>
2022-09-09 11:23:35 -0400

    Added icon support for link buttons

--- projects/figma-code/templates/components/link-button/link-button.php
+++ projects/figma-code/templates/components/link-button/link-button.php
@@ -1,8 +1,19 @@
 <?php 
-	$leftIcon = $button['left-icon'] ?? "";
-	$rightIcon = $button['right-icon'] ?? "";
+	$leftIcon = $button['left-icon'] ?? false;
+	$rightIcon = $button['right-icon'] ?? false;
 ?>
 
 <a href="?page=<?=$name?>" class="button <?=$classes?>">
+	<?php 
+		if ($leftIcon) {
+			echo "<picture><img src='images/$leftIcon' alt=''></picture>";
+		}
+	?>
 	<span><?=$name?></span>
+	<?php 
+		if ($rightIcon) {
+			echo "<picture><img src='images/$rightIcon' alt=''></picture>";
+		}
+	?>
 </a>
+
