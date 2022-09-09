a7f73e0518199b30ac8692e789568b653bc49f45
beejsbj <burooj.bj@gmail.com>
2022-09-08 23:44:21 -0400

    Init hero module

---
+++ projects/figma-code/templates/modules/hero-module/hero-module.php
@@ -0,0 +1,10 @@
+<hero-module>
+	<h1 class="loud-voice">
+		Arcu suscipit massa aliquam proin amet
+	</h1>
+	<p class="calm-voice">
+		Luctus felis sit lectus tristique diam ornare bibendum. Arcu auctor suspendisse luctus amet bibendum pellentesque lorem. Malesuada lobortis tristique tortor
+	</p>
+	<?php include('templates/components/text-content/text-content.php'); ?>
+	<?php include('templates/components/buttons/buttons.php'); ?>
+</hero-module>
