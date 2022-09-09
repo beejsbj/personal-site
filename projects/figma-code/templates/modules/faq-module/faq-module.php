1bf3032c53aa7e8d8701b00db0fe243d3d6ee833
beejsbj <burooj.bj@gmail.com>
2022-09-09 11:06:55 -0400

    Created Faq module

---
+++ projects/figma-code/templates/modules/faq-module/faq-module.php
@@ -0,0 +1,9 @@
+
+
+
+
+<faq-module>
+	<?php include('templates/components/text-content/text-content.php'); ?>
+	<?php include('templates/components/details-content/details-content.php'); ?>
+
+</faq-module>
