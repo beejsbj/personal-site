09bd8a4adac532addfcd6f2ada6eb57bbd38bccb
beejsbj <burooj.bj@gmail.com>
2022-09-09 11:07:35 -0400

    Created sitemap component for footer module

--- projects/figma-code/templates/modules/footer/footer.php
+++ projects/figma-code/templates/modules/footer/footer.php
@@ -1,6 +1,84 @@
 <footer>
 	<inner-column>
 		<h2 class="attention-voice">Zee footah</h2>
+		<site-map>
+			<nav>
+				<h1 class="solid-voice">
+					Product
+				</h1>
+				<ul>
+					<li>
+						Diam orci
+					</li>
+					<li>
+						Diam orci viverra
+					</li>
+					<li>
+						Suspendisse viv
+					</li>
+					<li>
+						Suspendisse viverra
+					</li>
+					<li>
+						Diam orci
+					</li>
+					<li>
+						Vuspendisse
+					</li>
+				</ul>
+			</nav>
+			<nav>
+				<h1 class="solid-voice">
+					Information
+				</h1>
+				<ul>
+					<li>
+						Diam orci
+					</li>
+					<li>
+						Diam viverra
+					</li>
+					<li>
+						Suspendisse viv
+					</li>
+				</ul>
+			</nav>
+			<nav>
+				<h1 class="solid-voice">
+					Information
+				</h1>
+				<ul>
+					<li>
+						Diam orci
+					</li>
+					<li>
+						Diam viverra
+					</li>
+					<li>
+						Suspe viv
+					</li>
+					<li>
+						Suspendisse viv
+					</li>
+				</ul>
+			</nav>
+			<subscribe-box>
+				<h1 class="solid-voice">
+					Subscribe
+				</h1>
+				<?php include('templates/components/email-signup/email-signup.php'); ?>
+				<p class="calm-voice">
+					
+				</p>
+			</subscribe-box>
+		</site-map>
+		<mast-foot>
+			<picture class="logo">
+				<?php include('images/logo.php'); ?>
+			</picture>
+			<?php include('templates/components/site-menu/site-menu.php'); ?>
+			<?php include('templates/components/buttons/buttons.php'); ?>
+		</mast-foot>
 	</inner-column>
 </footer>
 </body>
