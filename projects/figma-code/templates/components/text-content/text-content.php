4a27ca113ba349fe6d0e47cfc314efedabd216e2
beejsbj <burooj.bj@gmail.com>
2022-09-08 23:43:32 -0400

    Ctrate text-content module

---
+++ projects/figma-code/templates/components/text-content/text-content.php
@@ -0,0 +1,14 @@
+<?php
+	$textContents = $components['text-content'];
+
+	foreach ($textContents as $textContent) {
+		$element = $textContent['element'];
+		$content = $textContent['content'];
+		$classes = implode(" ", $textContent['classes']); 
+		?>
+
+		<<?=$element?> class="<?=$classes?>">
+			<?=$content?>
+		</<?=$element?>>
+
+	<?php } ?>
