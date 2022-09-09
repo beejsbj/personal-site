a84ed2acddcbed96df05ade649797f5a2fa37c82
beejsbj <burooj.bj@gmail.com>
2022-09-08 23:43:49 -0400

    Add header module

---
+++ projects/figma-code/templates/modules/header/header.php
@@ -0,0 +1,19 @@
+
+<!doctype html>
+<html lang='en' class='special-magic no-js <?=$pageData['id']?>'>
+
+	<head>
+		<meta charset='utf-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+		<title><?=$pageData['title']?></title>
+		<meta name="description" content="Adventureeee">
+		<meta property="og:image" content="https://thumbs.dreamstime.com/b/woman-stands-crossroads-two-forest-roads-girl-choice-path-lost-woods-ways-to-achieve-goal-157422242.jpg">
+		<link rel='stylesheet' href='styles/site.css'>
+	</head>
+
+	<body>
+		<header>
+			<inner-column>
+				<?php include('templates/modules/mast-head/mast-head.php'); ?>
+			</inner-column>
+		</header>
