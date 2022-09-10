1ba4304ec18da21b2ff389470cedd021c6da198f
beejsbj <burooj.bj@gmail.com>
2022-09-10 14:48:57 -0400

    Styling cta module for 2 and 3

--- projects/figma-code/templates/modules/cta-module/cta-module.php
+++ projects/figma-code/templates/modules/cta-module/cta-module.php
@@ -1,4 +1,8 @@
 <cta-module>
 	<?php include('templates/components/text-content/text-content.php'); ?>
+	<picture class="square">
+		<img src="images/square.jpg" alt="">
+	</picture>
 	<?php include('templates/components/buttons/buttons.php'); ?>
+	
 </cta-module>
