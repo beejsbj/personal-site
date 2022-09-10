64539899b4ca1a4a83cb0c4bd20ec793312f40fb
beejsbj <burooj.bj@gmail.com>
2022-09-09 20:43:06 -0400

    Styling footer

--- projects/figma-code/templates/modules/footer/footer.php
+++ projects/figma-code/templates/modules/footer/footer.php
@@ -1,74 +1,44 @@
+<?php 
+$footerJson = file_get_contents("data/modules/footer.json"); $footerData = json_decode($footerJson, true); $components = $footerData['components'];
+
+?>
 <footer>
 	<inner-column>
-		<h2 class="attention-voice">Zee footah</h2>
 		<site-map>
 			<nav>
-				<h3 class="solid-voice">
-					Product
-				</h3>
+				<h3 class="solid-voice"> Product </h3>
 				<ul>
-					<li>
-						Diam orci
-					</li>
-					<li>
-						Diam orci viverra
-					</li>
-					<li>
-						Suspendisse viv
-					</li>
-					<li>
-						Suspendisse viverra
-					</li>
-					<li>
-						Diam orci
-					</li>
-					<li>
-						Vuspendisse
-					</li>
+					<li> Diam orci </li>
+					<li> Diam orci viverra </li>
+					<li> Suspendisse viv </li>
+					<li> Suspendisse viverra </li>
+					<li> Diam orci </li>
+					<li> Vuspendisse </li>
 				</ul>
 			</nav>
 			<nav>
-				<h3 class="solid-voice">
-					Information
-				</h3>
+				<h3 class="solid-voice"> Information </h3>
 				<ul>
-					<li>
-						Diam orci
-					</li>
-					<li>
-						Diam viverra
-					</li>
-					<li>
-						Suspendisse viv
-					</li>
+					<li> Diam orci </li>
+					<li> Diam viverra </li>
+					<li> Suspe viv </li>
+					<li> Suspendisse viv </li>
 				</ul>
 			</nav>
 			<nav>
-				<h3 class="solid-voice">
-					Information
-				</h3>
+				<h3 class="solid-voice"> Information </h3>
 				<ul>
-					<li>
-						Diam orci
-					</li>
-					<li>
-						Diam viverra
-					</li>
-					<li>
-						Suspe viv
-					</li>
-					<li>
-						Suspendisse viv
-					</li>
+					<li> Diam orci </li>
+					<li> Diam viverra </li>
+					<li> Suspendisse viv </li>
 				</ul>
 			</nav>
+			
 			<subscribe-box>
-				<h3 class="solid-voice">
-					Subscribe
-				</h3>
+				<h3 class="solid-voice"> Subscribe </h3>
 				<?php include('templates/components/email-signup/email-signup.php'); ?>
-				<p class="calm-voice">
-					
+				<p class="whisper-voice">
+					Gravida sed justo, justo, id est et. Amet tristique convallis sed porttitor nullam eu ut. Duis et odio aliquam bibendum. Metus et lectus id viverra fringilla magna morbi. 
 				</p>
 			</subscribe-box>
 		</site-map>
