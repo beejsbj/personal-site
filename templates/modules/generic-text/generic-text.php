0b500a3bd4c8de140152ce944d4d3e19a851ceb6
beejsbj <burooj.bj@gmail.com>
2022-09-27 18:57:10 -0400

    misc fix

--- templates/modules/generic-text/generic-text.php
+++ templates/modules/generic-text/generic-text.php
@@ -1,13 +1,13 @@
 <?php 
 // if heading exists condition
 	$sectionHeading = $section['heading'] ?? "This is the Generic text module";
-	$paragraph = $section['intro'] ?? "this is the generic text paragraph";
+	$paragraph = $section['paragraph'] ?? "this is the generic text paragraph";
 ?>
 
-<h2 class="attention-voice">
+<h2 class="loud-voice">
 	<?=$sectionHeading?>
 </h2>
 
-<p class="calm-voice">
+<p class="attention-voice">
 	<?=$paragraph?>
 </p>
