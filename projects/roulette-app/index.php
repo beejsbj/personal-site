f7f1ca54fdcf902262f90bcf773fa362c287109e
beejsbj <burooj.bj@gmail.com>
2022-10-10 06:29:11 +0300

    misc

--- projects/roulette-app/index.php
+++ projects/roulette-app/index.php
@@ -16,15 +16,107 @@
 				<button class="button connect">
 					<picture class="metamask-icon">
 						<img src="metamask-bw.png" alt="">
-					</picture> 
+					</picture>
 					<span>Connect</span>
 				</button>
 				<inner-column>
-					<h1 class="loud-voice slide-in-top">Lottery. api3</h1>
-					<wheel-module>
-						<?php include 'wheel.php'; ?>
-					</wheel-module>
-					
+					<h1 class="loud-voice slide-in-top">Roullete</h1>
+					<roullete-module>
+						<wheel-module>
+							<?php include 'wheel.php'; ?>
+							<button class="button" id="spin"> Spin </button>
+						</wheel-module>
+						<numbers-grid>
+							<ul class="numbers">
+								<?php for ($i=1; $i <= 36; $i++) { ?>
+								<li>
+									<input type="checkbox" id="num-<?=$i?>" class="num" value="<?=$i?>">
+									<label for="num-<?=$i?>">
+										<span>
+											<svg class="svg-text" width="100%" height="100%" viewBox="-120 0 200 400">
+												<defs>
+													<filter id="fil2" x="0" y="0">
+														<feGaussianBlur in="SourceGraphic" stdDeviation="5" />
+													</filter>
+												</defs>
+												<g transform="matrix(1,0,0,1,-348.834,-160.526)">
+													<g transform="matrix(1,0,0,1,128.575,14.4738)">
+														<text class="back-text" text-anchor="middle" x="100%" y="100%">
+															<?=$i?>
+														</text>
+													</g>
+													<g transform="matrix(1,0,0,1,128.575,14.4738)">
+														<text class="front-text" text-anchor="middle" x="100%" y="100%">
+															<?=$i?>
+														</text>
+													</g>
+												</g>
+											</svg>
+										</span>
+									</label>
+								</li>
+								<?php }	?>
+							</ul>
+							<ul class="chunks halves-chunk">
+								<li>
+									<input type="checkbox" id="first-half" class="halves" value="firsthalf">
+									<label for="first-half">
+										<span> 1-&nbsp18 </span>
+									</label>
+								</li>
+								<li>
+									<input type="checkbox" id="even" class="halves" value="even">
+									<label for="even">
+										<span> even </span>
+									</label>
+								</li>
+								<li>
+									<input type="checkbox" id="red" class="halves" value="red">
+									<label for="red">
+										<span> red </span>
+									</label>
+								</li>
+								<li>
+									<input type="checkbox" id="blue" class="halves" value="blue">
+									<label for="blue">
+										<span> blue </span>
+									</label>
+								</li>
+								<li>
+									<input type="checkbox" id="odd" class="halves" value="odd">
+									<label for="odd">
+										<span> odd </span>
+									</label>
+								</li>
+								<li>
+									<input type="checkbox" id="second-half" class="halves" value="secondhalf">
+									<label for="second-half">
+										<span> 19-&nbsp36 </span>
+									</label>
+								</li>
+							</ul>
+							<ul class="chunks thirds-chunk">
+								<li class="thirds">
+									<input type="checkbox" id="first-12" value="first12">
+									<label for="first-12">
+										<span> First&nbspThird </span>
+									</label>
+								</li>
+								<li class="thirds">
+									<input type="checkbox" id="second-12" value="second12">
+									<label for="second-12">
+										<span> second&nbspThird </span>
+									</label>
+								</li>
+								<li class="thirds">
+									<input type="checkbox" id="third-12" value="third12">
+									<label for="third-12">
+										<span> third&nbspThird </span>
+									</label>
+								</li>
+							</ul>
+						</numbers-grid>
+					</roullete-module>
 				</inner-column>
 			</section>
 		</main>
--- projects/roulette-app/scripts/wheel.js
+++ projects/roulette-app/scripts/wheel.js
@@ -1,47 +1,60 @@
-
-
-
-
-var start = Date.now(); // remember start time
-var i = Math.floor(Math.random() * 37);
-
-
-function animations(){
+function animations(start) {
+	
 	// how much time passed from the start?
 	var timePassed = Date.now() - start;
+	if (timePassed >= 5000) {
+		clearInterval(animations); // finish the animation after 2 seconds
+		return;
+	}
+	var i = Math.floor(Math.random() * 37);
 	i++;
 	if (i == 37) {
 		i = 1;
 	}
-  
-
-  if (timePassed >= 5000) {
-    clearInterval(animations); // finish the animation after 2 seconds
-    return;
-  }
-
-
-
-  // highlight the animation at the moment timePassed
-  highlight(timePassed, i);
+	// highlight the animation at the moment timePassed
+	highlight(timePassed, i);
 }
-
-
-
 // as timePassed goes from 0 to 2000
 // left gets values from 0px to 400px
 function highlight(timePassed, i) {
-	var sliceName = `svg #slice${i}`;
-	var currentSlice = document.querySelector(sliceName);
-	console.log(sliceName);
+	var whiteSliceName = `#white-slices #slice-${i}`;
+	var colorSliceName = `#color-slices #slice-${i}-color`;
+	var currentWhiteSlice = document.querySelector(whiteSliceName);
+	var currentColorSlice = document.querySelector(colorSliceName);
+	// console.log(i);
+	currentWhiteSlice.classList.toggle('selected');
+	currentColorSlice.classList.toggle('selected');
 
- 	currentSlice.classList.toggle('selected');
+	if (timePassed < 4800) {
+		setTimeout(function() {
+			currentWhiteSlice.classList.toggle('selected');
+			currentColorSlice.classList.toggle('selected');
+		}, 200);
+	}
+}
 
-	setTimeout(function () {
-		currentSlice.classList.toggle('selected');
-	}, 50);
+var spinButton = document.querySelector('button#spin');
+spinButton.addEventListener("click", function() {
+	removeSLiceClass();
+	var start = Date.now(); // remember start time
+	setInterval(animations, 200, start);
+});
+
+
+
+function removeSLiceClass() {
+	var selectedSlices = document.querySelectorAll(".selected");
+
+	selectedSlices.forEach(function(slice) {
+		console.log(slice);
+		slice.classList.remove("selected");
+		console.log(slice);
+	})
 }
 
 
 
-setInterval(animations, 100);
+
+//number chart
+
+
