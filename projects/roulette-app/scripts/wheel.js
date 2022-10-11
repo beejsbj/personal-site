f93fe0536fa8170b76e219b9598ae8d33e576caf
beejsbj <burooj.bj@gmail.com>
2022-10-11 17:24:56 +0300

    added wheel changing effects

--- projects/roulette-app/scripts/wheel.js
+++ projects/roulette-app/scripts/wheel.js
@@ -1,5 +1,19 @@
 const spinTime = 5000;
 var frameTime = 150;
+const blueNums = [3, 32, 19, 21, 25, 34, 27, 36, 30, 23, 5, 16, 1, 14, 9, 18, 7, 12];
+
+function wheelStrokeColor(i) {
+	var allStrokes = document.querySelectorAll('#color-slices path');
+	if (blueNums.includes(i)) {
+		allStrokes.forEach(function(stroke) {
+			stroke.style.fill = "var(--blue)";
+		})
+	} else {
+		allStrokes.forEach(function(stroke) {
+			stroke.style.fill = "var(--red)";
+		})
+	}
+}
 
 function animations(start) {
 	
@@ -20,6 +34,7 @@ function animations(start) {
 // as timePassed goes from 0 to frameTime0
 // left gets values from 0px to 400px
 function highlight(timePassed, i) {
+	wheelStrokeColor(i);
 	var whiteSliceName = `#white-slices #slice-${i}`;
 	var colorSliceName = `#color-slices #slice-${i}-color`;
 	var currentWhiteSlice = document.querySelector(whiteSliceName);
@@ -51,9 +66,9 @@ function removeSLiceClass() {
 	var selectedSlices = document.querySelectorAll(".selected");
 
 	selectedSlices.forEach(function(slice) {
-		console.log(slice);
+		// console.log(slice);
 		slice.classList.remove("selected");
-		console.log(slice);
+		// console.log(slice);
 	})
 }
 
@@ -63,12 +78,10 @@ function removeSLiceClass() {
 //number chart styling
 
 
-const blueNums = [3, 32, 19, 21, 25, 34, 27, 36, 30, 23, 5, 16, 1, 14, 9, 18, 7, 12];
+
 
 blueNums.forEach( function(blueNum) {
-	console.log(blueNum);
 	var blueCheckbox = document.querySelector(`#num-${blueNum} + label`);
-	console.log(blueCheckbox);
 	blueCheckbox.style.borderColor = "var(--blue)";
 	blueCheckbox.style.boxShadow = "0px 0px 5px var(--blue)";
 })
@@ -80,11 +93,11 @@ blueNums.forEach( function(blueNum) {
 
 //animation flicker
 
-var titleSign = document.querySelector(`h1 path:nth-of-type(${Math.floor(Math.random() * 8)})`)
+var titleSign = document.querySelector(`h1 path:nth-of-type(${Math.floor(Math.random() * 8) + 1})`)
 titleSign.classList.add('flicker-animation');
 
-var titleSign2 = document.querySelector(`h1 path:nth-of-type(${Math.floor(Math.random() * 8)})`)
+var titleSign2 = document.querySelector(`h1 path:nth-of-type(${Math.floor(Math.random() * 8) + 1})`)
 titleSign2.classList.add('flicker-animation2');
 
-var spinSign = document.querySelector(`#back-spin path:nth-of-type(${Math.floor(Math.random() * 4)})`)
+var spinSign = document.querySelector(`#back-spin path:nth-of-type(${Math.floor(Math.random() * 4) + 1})`)
 spinSign.classList.add('flicker-animation');
