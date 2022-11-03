777c758fe1484ca9a7c727a5e36e0a10185f5614
beejsbj <burooj.bj@gmail.com>
2022-11-03 19:43:21 -0400

    add new enclosing elements

--- templates/modules/exercise-detail/exercise-detail.php
+++ templates/modules/exercise-detail/exercise-detail.php
@@ -1,18 +1,21 @@
 <?php 
 // if heading exists condition
 	$exLink = $_GET['exercise'] ?? "saying-hello";
-	$exerciseName = unDasher($exLink);
-	$funcName = functionNamer($exerciseName);
+	$exerciseName = kebabToCapital($exLink);
+	$funcName = getCamelCase($exerciseName);
 ?>
 <exercise-detail>
-	<a href="?page=e4p">BACK</a>
+	<actions-component>
+		<a href="?page=e4p"><span>BACK</span></a>
+		<toggle-switch>
+			<input type="checkbox" id="php-js">
+			<label for="php-js"></label>
+		</toggle-switch>
+	</actions-component>
 	<h2 class="loud-voice">
 		<?=$exerciseName?>
 	</h2>
-	<toggle-switch>
-		<input type="checkbox" id="php-js">
-		<label for="php-js"></label>
-	</toggle-switch>
+	
 	<div>
 		<h3 class="attention-voice">PHP</h3>
 		<?php include("projects/e4p/$exLink.php"); ?>
@@ -21,17 +24,17 @@
 
 <script src="projects/e4p/e4p.js"></script>
 <script>
-	var toggleSwitch = document.querySelector('exercise-detail toggle-switch input');
-	var button = document.querySelector('form#e4p button');
+	var $toggleSwitch = document.querySelector('exercise-detail toggle-switch input');
+	var $form = document.querySelector('form#e4p');
 
 
-	toggleSwitch.addEventListener('input', function() {
+	$toggleSwitch.addEventListener('input', function() {
 		var heading = document.querySelector('exercise-detail > div h3');
 		var $outlet = document.querySelector('div#e4p-output');
 
-		if (toggleSwitch.checked) {
+		if ($toggleSwitch.checked) {
 			heading.innerHTML = 'JS';
-			button.addEventListener('click', function(event) {
+			$form.addEventListener('submit', function(event) {
 				event.preventDefault();
 
 				<?=$funcName?>($outlet);
