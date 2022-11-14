86952f85b3c6ca91e18b714353b10efa6ecfdc25
beejsbj <burooj.bj@gmail.com>
2022-11-14 13:11:43 -0500

    Created Standard figure module

---
+++ templates/modules/standard-figure/standard-figure.php
@@ -0,0 +1,14 @@
+<?php 
+ ?>
+
+<standard-figure>
+	<figure>
+		<picture>
+			<img src="images/square.jpg" alt="">
+		</picture>
+		<figcaption>
+			this is the caption
+		</figcaption>
+	</figure>
+	<p>description of figure and why it matter</p>
+</standard-figure>
