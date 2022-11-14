4585c6972783c0ea41a1b86dc470a8dff31f7412
beejsbj <burooj.bj@gmail.com>
2022-11-14 13:12:25 -0500

    Created module to generate menus from data

---
+++ templates/modules/menu-module/menu-module.php
@@ -0,0 +1,34 @@
+
+<?php
+	$menu = $section['menu'] ?? $menu; 
+
+	$menuJson = file_get_contents("data/components/$menu-menu.json");
+	$menuData = json_decode($menuJson, true);
+	 
+?>
+
+<nav class="<?=$menu?>-menu">
+	<ul>
+		<?php foreach ($menuData as $menuItem) {
+			$class = $menuItem['class'] ?? ""; 
+			$slug = $menuItem['slug']; 
+			if ($menu == 'site') {
+				$class = activePage($menuItem['class']);
+				$slug = $menuItem['slug'] . "&theme=" . currentTheme();
+			}
+			if ($menu == 'theme') {
+				$class = activeTheme($menuItem['class']);
+				$slug = "?page=" . currentPage() . $menuItem['slug'];
+			}
+			?>
+			<li>
+				<a 
+					class="<?=$class?>" 
+					href="<?=$slug?>"
+				>
+					<span><?=$menuItem['name']?></span>
+				</a>
+			</li>
+		<?php } ?>
+	</ul>
+</nav>
