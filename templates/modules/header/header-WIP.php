d7b81d8de20b2db0c4fde550a269adc164ae6218
beejsbj <burooj.bj@gmail.com>
2022-11-14 13:10:08 -0500

    Refactored Header to be data backed. WIP

---
+++ templates/modules/header/header-WIP.php
@@ -0,0 +1,27 @@
+<header>
+<?php
+	$headerJson = file_get_contents("data/modules/header.json");
+	$headerData = json_decode($headerJson, true);
+	$sections = $headerData;
+	$sectionHeading = $pageData['page-title'];
+	foreach ($sections as $section) {
+		$module = $section['module'] ?? "generic-text";
+?>
+	<section class="header-section <?=$module?>">
+		<inner-column>
+			<?php include("templates/modules/$module/$module.php"); ?>
+		</inner-column>
+	</section>
+<?php } ?>
+
+
+
+
+	<svg class="circle" viewBox="0 0 100 100">
+	  <circle cx="50" cy="50" r="40" />
+	</svg>
+
+	
+
+	
+</header>
