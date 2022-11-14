420b4e7c441aada1996e39b1c20b7040d3388d9a
beejsbj <burooj.bj@gmail.com>
2022-11-14 13:14:30 -0500

    Refactored About module into smaller peices

--- templates/modules/about-module/about-module.php
+++ templates/modules/about-module/about-module.php
@@ -3,26 +3,6 @@
 	$sectionHeading = $section['heading'] ?? "This is the About module";
 ?>
 <about-module>
-	<text-content>
-		<h2 class="attention-voice">
-			<?=$sectionHeading?>
-		</h2>
-		<?php
-	$paragraphs = $section['paragraphs'] ?? ["this is about paragraph","this is about paragraph"]; 
-	foreach ($paragraphs as $paragraph) { ?>
-		<p class="calm-voice">
-			<?=$paragraph?>
-		</p>
-	
-	<?php } ?>
-	</text-content>
-	<image-grid>
-		<?php
-		$images = $section['images']  ?? ['square.jpg', 'square.jpg']; 
-		foreach ($images as $image) { ?>
-		<picture>
-			<img src="images/<?=$image?>" alt="">
-		</picture>
-		<?php } ?>
-	</image-grid>
+	<?php include('templates/modules/generic-text/generic-text.php') ?>
+	<?php include('templates/modules/image-grid/image-grid.php') ?>
 </about-module>
