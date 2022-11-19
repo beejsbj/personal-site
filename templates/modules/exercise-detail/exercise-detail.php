c912c51a077bc3d236c684f2d0f98382e6ddafd7
beejsbj <burooj.bj@gmail.com>
2022-11-19 11:29:33 -0500

    modified exercise detail page to allow for vue

--- templates/modules/exercise-detail/exercise-detail.php
+++ templates/modules/exercise-detail/exercise-detail.php
@@ -8,8 +8,19 @@
 	<actions-component>
 		<a href="?page=e4p"><span>BACK</span></a>
 		<toggle-switch>
-			<input type="checkbox" id="php-js">
-			<label for="php-js"></label>
+			<radio-option>
+				<input type="radio"  name="e4p-lang" value="php" id="option-php">
+				<label for="option-php">php</label>
+			</radio-option>
+			<radio-option>
+				<input type="radio" name="e4p-lang" value="js"  id="option-js">
+				<label for="option-js">js</label>
+			</radio-option>
+			<radio-option>
+				<input type="radio" name="e4p-lang" value="vue"  id="option-vue">
+				<label for="option-vue">vue</label>
+			</radio-option>
+
 		</toggle-switch>
 	</actions-component>
 	<h2 class="loud-voice">
@@ -24,30 +35,58 @@
 
 	
 <script src="projects/e4p/e4p.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.7.13/dist/vue.js"></script>
+<script src="projects/e4p/vue-e4p.js"></script>
 <script>
 	var $form = document.querySelector('form#e4p');
 	var $heading = document.querySelector('exercise-detail > exercise-form h3');
 	var $outlet = document.querySelector('div#e4p-output');
-	var $toggleSwitch = document.querySelector('exercise-detail toggle-switch input');
-
-	$toggleSwitch.checked = localStorage.toggleSwitch
+	var $toggleSwitch = document.querySelector('exercise-detail toggle-switch');
+	var $submit = document.querySelector("#e4p button");
+	var checkedId = localStorage.toggleSwitch
 		? JSON.parse(localStorage.toggleSwitch)
-		: false;
+		: 'option-php';
+
+	var $checkedSwitch = document.querySelector(`exercise-detail toggle-switch #${checkedId}`);
+	$checkedSwitch.checked = true;
 	toggleRunner();
 	$toggleSwitch.addEventListener('input', toggleRunner)
-	$form.addEventListener('submit', function (event) {
-			if ($toggleSwitch.checked) {
-				event.preventDefault();
-				<?=$funcName?>($outlet);
-			} else {
-				$form.submit();
-			}
-		})
+
+	
 	function toggleRunner() {
-		localStorage.toggleSwitch = JSON.stringify( $toggleSwitch.checked );
-		$heading.innerHTML = ($toggleSwitch.checked) ? 'JS' : 'PHP';
+		$checkedSwitch = $toggleSwitch.querySelector( 'input:checked' );
+		localStorage.toggleSwitch = JSON.stringify( $checkedSwitch.id );
+		$heading.innerHTML = $checkedSwitch.value;
+		if ( $checkedSwitch.value == 'vue' ) {
+			initInputs();
+			$outlet.innerHTML = `{{output}}`;
+			$form.classList.add('vue');
+			exercise.current = new Vue(V<?=$funcName?>);
+		} else {
+			if (exercise.current) exercise.current.$destroy();
+
+			if (!$form.dataset.handler) {
+				$form.dataset.handler = 'true';
+				$form.addEventListener('submit', function ( event ) {
+					if ($checkedSwitch.value != 'php') {
+						event.preventDefault();
+
+	
+					} else {
+				 		$form.submit();
+					}
+					if ($checkedSwitch.value == 'js') {
+						<?=$funcName?>($outlet);
+					} else if ($checkedSwitch.value == 'vue'){
+						console.log('hi');
+					} 
+				})
+			}
+			
+		}
 		
 	}
+
 	
 </script>
 
