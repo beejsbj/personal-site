b8482fc70ecaa8641b96f4b4dc907bb16691a3fb
beejsbj <burooj.bj@gmail.com>
2022-11-20 19:12:44 -0500

    Turned shape into its own ifle

---
+++ projects/e4p/shape.php
@@ -0,0 +1,12 @@
+<div class="shape-wrapper">
+    <div class="shape red-fill jelly">
+        <svg x="0px" y="0px" viewBox="0 0 108.1 47" enable-background="new 0 0 108.1 47">
+            <polygon fill="#FF0000" points="19.5,0,110.7,0,80.1,32.7,3.1,47 " />
+        </svg>
+    </div>
+    <div class="shape cyan-fill jellyalt">
+        <svg x="0px" y="0px" viewBox="0 0 108.1 47" enable-background="new 0 0 108.1 47">
+            <polygon fill="#00FFFF" points="11,3,85.1,0 118.8,45.6,14.3,29 " />
+        </svg>
+    </div>
+</div>
