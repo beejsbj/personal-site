1bf8815612a065f8df2536d757322962d9e824e7
beejsbj <burooj.bj@gmail.com>
2022-12-13 09:56:04 -0500

    Started the app, working

--- projects/monsters-ejs/follow-along/app.js
+++ projects/monsters-ejs/follow-along/app.js
@@ -1,3 +1,14 @@
 import express from 'express';
 
-const app = express();
+const app = express(); 
+const PORT = 1996;
+
+
+app.get('/', function( request, response ){
+	response.send('<h1>Hello</h1>')
+})
+
+
+app.listen(PORT, function(){
+	console.log('listening to port')
+})
