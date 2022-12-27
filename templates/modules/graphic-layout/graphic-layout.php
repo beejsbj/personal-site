e8407f390d342fb545e359d29d5611706fc05249
beejsbj <burooj.bj@gmail.com>
2022-12-26 20:34:38 -0500

    Created module, WIP

---
+++ templates/modules/graphic-layout/graphic-layout.php
@@ -0,0 +1,9 @@
+<graphic-layout>
+		<picture>
+		<img src="images/landscape.jpg">
+	</picture>
+	<picture class="hide-below-900">
+		<img src="images/landscape.jpg">
+	</picture>
+	<p class="hide-below-900">hidden bonus secret text only at larger sizes cause mobile sucks. hoho.</p>
+	</graphic-layout>
