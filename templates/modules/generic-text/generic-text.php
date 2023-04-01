f23dce633bbef738ae47cd8e5da4433a5f31c41f
beejsbj <burooj.bj@gmail.com>
2023-04-01 18:00:13 -0400

    Reworking generic text module

--- templates/modules/generic-text/generic-text.php
+++ templates/modules/generic-text/generic-text.php
@@ -1,18 +1,23 @@
+<?php
 
-<?php 
-// if heading exists condition
-	$sectionHeading = $sectionHeading ?? ($section['heading'] ?? "This is the Generic text module");
-	
-	$headingClass = $section['heading-class'] ?? "attention-voice";
-	$paragraphs = $section['paragraphs'] ?? ["this is the generic text paragraph" => "hide"];
+$sectionHeading = $section['heading'] ?? $sectionHeading ?? "this is the generic text heading";
+
+$headingClass = $section['heading-class'] ?? "attention-voice";
+$paragraphsClass = $section['paragraphsClass'] ?? "hide";
+$paragraphs = $section['paragraphs'] ?? ["this is the generic text paragraph" => "hide"];
+
 ?>
 <text-content>
-	<h2 class="<?=$headingClass?>">
-		<?=$sectionHeading?>
+	<h2 class="<?= $headingClass ?>">
+		<?= $sectionHeading ?>
 	</h2>
-<?php foreach ($paragraphs as $paragraph => $textVoice) { ?>
-	<p class="<?=$textVoice?>">
-		<?=$paragraph?>
-	</p>
-<?php } ?>
+	<?php foreach ($paragraphs as $paragraph) {
+		$textVoice =   $paragraph['voice'] ?? $paragraphsClass ?? "hide";
+		$paragraph = is_array($paragraph) ?  $paragraph['text'] : $paragraph;
+	?>
+		<p class="<?= $textVoice ?>">
+			<?= $paragraph ?>
+		</p>
+	<?php } ?>
+
 </text-content>
