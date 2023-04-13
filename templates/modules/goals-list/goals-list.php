f77a95ae067ed921fb0e06063d71d0fe3c014eef
beejsbj <burooj.bj@gmail.com>
2023-04-13 14:47:15 -0400

    Style goals page

--- templates/modules/goals-list/goals-list.php
+++ templates/modules/goals-list/goals-list.php
@@ -1,19 +1,19 @@
-<?php 
+<?php
 // if heading exists condition
-	$sectionHeading = $section['heading'] ?? "This is the goals module";
+$sectionHeading = $section['heading'] ?? "This is the goals module";
 ?>
 
 
 <goals-module>
-	<h2 class="attention-voice">
-	<?=$sectionHeading?>
-</h2>
-<ul >
+	<h2 class="loud-voice">
+		<?= $sectionHeading ?>
+	</h2>
+	<ul>
 
-	<?php
+		<?php
 		$goals = $section['goals'] ?? ["This is a goal", "This is a goal"];
-	 	foreach ($goals as $goal) {
+		foreach ($goals as $goal) {
 			echo "<li>$goal</li>";
 		} ?>
-</ul>
+	</ul>
 </goals-module>
