be8ad68cf2df5a4af21d96aabd45b359beb29f16
beejsbj <burooj.bj@gmail.com>
2023-08-20 16:41:30 -0400

    Add video frames for devices to show website

--- templates/modules/frame-image/frame-image.php
+++ templates/modules/frame-image/frame-image.php
@@ -1,15 +1,12 @@
 <?php
-
 $image = "images/projects/" . $section['image'] ?? "images/" . $pageData['image'] ?? 'images/landscape.jpg';
-$device = $section['device'] ?? "laptop";
-
+$device =  $section['device'] ?? "laptop";
 ?>
 
 
 <frame-image>
 	<picture class="<?= "$device-frame" ?>">
-		<img src="images/<?= "$device-frame.svg" ?>" style="
-			background-image: url('<?= $image ?>');
-			">
+		<img src="images/<?= "$device-frame.svg" ?>">
+		<video src="<?= "$image" ?>" autoplay loop></video>
 	</picture>
 </frame-image>
